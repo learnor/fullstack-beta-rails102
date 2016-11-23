ef9987a5f6935d4c4c2954bc04e5f869db132c05
Learnor <learnor@gmail.com>
2016-11-23 12:31:36 -0600

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to groups_path
+    if @group.save
+       redirect_to groups_path
+     else
+       render :new
+     end 
   end
 
   def show
@@ -26,7 +29,14 @@ class GroupsController < ApplicationController
     @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
   end
-  
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+
   private
 
   def group_params
