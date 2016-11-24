bc5f77eab524b0456c7fd8597c9d5dd400c3486b
Learnor <learnor@gmail.com>
2016-11-23 21:20:33 -0600

    add order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts
+    @posts = @group.posts.order("created_at DESC")
   end
 
   def edit
@@ -53,4 +53,5 @@ class GroupsController < ApplicationController
       redirect_to root_path, alert: "You have no permission."
     end
   end
+
   end
